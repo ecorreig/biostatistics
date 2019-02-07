f7625a6b461052b6ebd1193b380cf9d2b7634966
eudald <eudald@correig.net>
2019-02-07 14:34:15 +0100

    healthcare.ai

---
+++ models/healthcareai.R
@@ -0,0 +1,7 @@
+# WIP: Implemento aquí les funcionalitats de healthcare.ai, que tenen algunes eines útils
+
+
+require(healthcareai)
+
+
+models <- machine_learn(pima_diabetes, patient_id, outcome = diabetes)
